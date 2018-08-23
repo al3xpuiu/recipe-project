6b768df02e186d7ffa872503d4d3eae599208b91
Loky <puiualexandru87@yahoo.com>
2018-08-23 20:07:42 +0200

    Created Category class. Added many to many relationship between Category and Recipe. closes #1

---
+++ src/main/java/com/bishoptod3/domain/Category.java
@@ -0,0 +1,44 @@
+package com.bishoptod3.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Loky on 23/08/2018.
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
